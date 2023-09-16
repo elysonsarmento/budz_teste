966ab8b96460ba270eaf10ed2b6bc9864ee6b639
Elyson Sarmento <elyson@elyson.dev>
2023-09-16 14:43:59 -0300

    feat: :seedling: file for calling string icon

---
+++ lib/widgets/icons/icons_string.dart
@@ -0,0 +1,4 @@
+const String homeIcon = 'assets/icon/home.svg';
+const String smsIcon = 'assets/icon/sms.svg';
+const String personIcon = 'assets/icon/person.svg';
+const String gridIcon = 'assets/icon/grid_view.svg';
